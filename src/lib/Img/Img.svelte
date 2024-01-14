7b0bb0c4e9c9b6d1bd8a0d1548523b0bc8ce289b
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-01-14 08:01:32 +0530

    BZ-11848: feat: added support for fallback image in list item left icon
    - create Img component with support for handling fallback images
    - integrated Img component in ListItemComponent for leftIcon
    - added support for leftIconfallback to list item

---
+++ src/lib/Img/Img.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+  export let fallback: string | null = null;
+
+  function handleFallback(): void {
+    if (fallback !== null) {
+      src = fallback;
+    }
+  }
+</script>
+
+{#if typeof src === 'string' && typeof alt === 'string'}
+  <img {src} {alt} on:error|once={handleFallback} />
+{/if}
+
+<style>
+  img {
+    object-fit: var(--image-object-fit);
+    height: var(--image-height, 24px);
+    width: var(--image-width, 24px);
+    padding: var(--image-padding, 0px);
+    border-radius: var(--image-border-radius, 0px);
+    margin: var(--image-margin, 0px);
+    filter: var(--image-filter, none);
+    background: var(--image-background);
+    border: var(--image-border);
+    transition: var(--image-transition);
+  }
+
+  img:hover {
+    background: var(--image-hover-background, var(--image-background));
+    border: var(--image-hover-border, var(--image-border));
+  }
+</style>

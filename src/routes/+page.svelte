5d17f171f070bb88467cd4631c41697f1c8f89d9
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-09-23 16:06:07 +0530

    BZ-35005: feat: add grid item component
    - added new component GridItem
    - added example usage in page.svelte
    - updated dev script to use host

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,27 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import GridItem from '$lib/GridItem/GridItem.svelte';
+</script>
+
+<div class="container">
+  <h1>Svelte UI Components</h1>
+
+  <h3>Components</h3>
+
+  <div class="components">
+    <GridItem />
+  </div>
+</div>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
+
+  .container {
+    font-family: 'Nunito Sans', sans-serif;
+  }
+
+  .components {
+    display: flex;
+    gap: 16px;
+    flex-wrap: wrap;
+  }
+</style>

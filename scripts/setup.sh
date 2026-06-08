ed04e5d3898d4c5b0c52c2467e11898406397fbb
Sahil Sinha <sahilsinha.dar@gmail.com>
2026-06-08 16:12:38 +0530

    refactor: upgrade all packages & drop deperecated fixes
    - upgraded all dependencies to latest version
    - dropped deprecated packages & added appropriate replacements

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,7 @@
 chmod +x ./.husky/pre-commit
-chmod +x ./.husky/prepare-commit-msg
 chmod +x ./.husky/commit-msg
 chmod +x ./.husky/pre-push
 
 chmod +x ./scripts/pre-commit.sh
-chmod +x ./scripts/prepare-commit-msg.sh
 chmod +x ./scripts/commit-msg.sh
 chmod +x ./scripts/pre-push.sh
